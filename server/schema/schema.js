db5a1cb2e1c49b896b342dc1c105e71a0fe379f7
Gian Marco Forghieri <G.M.Forghieri@student.tudelft.nl>
2019-10-04 15:14:15 +0200

    Initial commit

---
+++ server/schema/schema.js
@@ -0,0 +1,22 @@
+const graphql = require('graphql');
+
+const{GraphQLOjectType, GraphQLString} = graphql;
+
+const BookType = new GraphQLOjectType({
+   name: 'Book',
+   fields: () => ({
+       id:{type: GraphQLString},
+       name:{type: GraphQLString},
+       genre:{type: GraphQLString},
+   })
+});
+
+const RootQuery = new GraphQLOjectType({
+    name:'RootQueryType',
+    fields: {
+       book: {
+           type: BookType,
+            args:{id:{type:GraphQLString}}
+       }
+    }
+});
