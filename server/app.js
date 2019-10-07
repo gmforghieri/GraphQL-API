e19807a7b9efd45f87ec5269cc7a3fe96ead279a
Gian Marco Forghieri <G.M.Forghieri@student.tudelft.nl>
2019-10-07 19:16:03 +0200

    setup MongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose
+    .connect('mongodb+srv://gmforghieri:3%23fa%25D5co58nX@cluster0-9mmzr.mongodb.net/test?retryWrites=true&w=majority', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('DB Connected!'))
+    .catch(err => {
+        console.log(Error, err.message);
+    });
 
 app.use('/graphql', graphqlHTTP({
     schema,
