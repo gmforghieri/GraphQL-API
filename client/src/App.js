bd4aa3c3d4226f3ea98776e3d81d908f3cb5bdef
Gian Marco Forghieri <G.M.Forghieri@student.tudelft.nl>
2019-10-07 23:28:45 +0200

    add Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import BookList from './components/BookList';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo'
+
+const client = new ApolloClient({
+    uri:'http://localhost:4000/grahpql'
+});
+
 
 class App extends Component {
     render() {
         return (
+            <ApolloProvider client={client}>
             <div id="main">
                 <h1>Reading List</h1>
                 <BookList />
             </div>
+            </ApolloProvider>
         );
     }
 }
